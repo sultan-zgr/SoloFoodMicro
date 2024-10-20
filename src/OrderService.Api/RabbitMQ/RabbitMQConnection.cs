7c2ccb98e58171103dcea02770edca7ba93f0828
Sultan Özgür <sultan-ozgur@hotmail.com>
2024-10-20 14:36:34 +0300

    fix rabbitmq

--- src/OrderService.Api/RabbitMQ/RabbitMQConnection.cs
+++ src/OrderService.Api/RabbitMQ/RabbitMQConnection.cs
@@ -1,34 +1,37 @@
 ﻿using RabbitMQ.Client;
+using System;
 
 namespace OrderService.Api.RabbitMQ
 {
-    public class RabbitMQConnection : IRabbitMQConnection
+    public class RabbitMQConnection : IRabbitMQConnection, IDisposable
     {
         private readonly IConnectionFactory _connectionFactory;
         private IConnection _connection;
         private bool _disposed;
 
-        public RabbitMQConnection()
+        public RabbitMQConnection(IConnectionFactory connectionFactory)
         {
-            _connectionFactory = new ConnectionFactory
-            {
-                HostName = "localhost"
-            };
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
         }
 
-        public bool IsConnected => _connection !=null && _connection.IsOpen &&!_disposed;
+        public bool IsConnected => _connection != null && _connection.IsOpen && !_disposed;
 
         public IModel CreateModel()
         {
-            if(!IsConnected) TryConnect();
+            if (!IsConnected)
+            {
+                throw new InvalidOperationException("No RabbitMQ connections are available to perform this action");
+            }
+
             return _connection.CreateModel();
         }
 
         public void Dispose()
         {
-            if(_disposed) return;
-            _connection?.Dispose();
+            if (_disposed) return;
             _disposed = true;
+            _connection?.Close();
+            _connection?.Dispose();
         }
 
         public bool TryConnect()
---
+++ tests/OrderService.Tests/Integration/OrderService.IntegrationTests/OrderPaymentIntegrationTests.cs
@@ -0,0 +1,23 @@
+﻿using OrderService.Api.RabbitMQ;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.IntegrationTests
+{
+    public class OrderPaymentIntegrationTests
+    {
+        private readonly IRabbitMQConnection _rabbitMQConnection;
+
+        public OrderPaymentIntegrationTests()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            _rabbitMQConnection = new RabbitMQConnection(factory);
+            _rabbitMQConnection.TryConnect(); // Bağlantıyı kontrol et ve kur
+
+        }
+    }
+}
